8870ea49e63ea29acf6b39f111762e5ec4dabe48
Nisarg Patel <nisargp42@gmail.com>
2020-10-03 00:46:35 -0400

    Static :: Design :: Done

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <template>
-  <div id="app">
-    <div class="min-h-screen bg-background-primary" :class="currentTheme">
-      <div class="text-tc-primary">
-        <!-- Theme Switcher icons with functionality -->
-        <app-theme-switcher></app-theme-switcher>
-        
-      </div>
+<div id="app">
+    <div class="bg-background-primary transition-background-25-ease-in" :class="currentTheme">
+        <div class="flex flex-col min-h-screen font-poppins font-medium text-tc-primary p-4">
+            <header>
+                <!-- Theme Switcher icons with functionality -->
+                <app-theme-switcher></app-theme-switcher>
+            </header>
+            <main class="flex flex-col flex-1 items-center justify-center">
+                <div>
+                    <!-- text-4xl larger viewport -->
+                    <h1 class="font-bold text-2xl uppercase">Country quiz</h1>
+                    <div class="flex flex-col bg-white mt-3 rounded-24px">
+                        <div class="text-right -mt-12">
+                            <img class="ml-auto w-32" src="@/assets/images/globe.svg" alt="Country quiz">
+                        </div>
+                        <div class="px-8 py-6">
+                            <h2 class="text-tc-secondary font-bold text-2xl">Kuala Lumpur is the capital of</h2>
+                            <div>
+                                <button class="flex items-center btn my-5">
+                                    <span class="text-base">A</span>
+                                    <span class="ml-8 text-sm">Vietnam</span>
+                                </button>
+                                <button class="flex items-center btn my-5">
+                                    <span class="text-base">A</span>
+                                    <span class="ml-8 text-sm">Vietnam</span>
+                                </button>
+                                <button class="flex items-center btn my-5">
+                                    <span class="text-base">A</span>
+                                    <span class="ml-8 text-sm">Vietnam</span>
+                                </button>
+                                <button class="flex items-center btn my-5">
+                                    <span class="text-base">A</span>
+                                    <span class="ml-8 text-sm">Vietnam</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="text-center my-8">
+                <p class="font-montserrat text-sm font-bold leading-4 text-gray-500">Nisarg Patel @ DevChallenges.io</p>
+            </footer>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 export default {
-  name: 'App',
-  components: {
-    'AppThemeSwitcher' : () => import('@/components/ThemeSwitcher.vue'),
-  },
-  computed  : {
-    ...mapState(['currentTheme'])
-  }
+    name: 'App',
+    components: {
+        'AppThemeSwitcher': () => import('@/components/ThemeSwitcher.vue'),
+    },
+    computed: {
+        ...mapState(['currentTheme'])
+    }
 }
 </script>
 
 <style lang="scss">
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
--- tailwind.js
+++ tailwind.js
@@ -15,9 +15,17 @@ module.exports = {
         },
         // Text colors : tc
         tc : {
-          primary : 'var(--text-tc-primary)'
-        }
-      }
+          primary : 'var(--text-tc-primary)',
+          secondary : 'var(--text-tc-secondary)',
+          ternary : 'var(--text-tc-ternary)',
+        },
+        //General
+        'purple' : 'rgba(96, 102, 208, 0.8)'
+      },
+      borderRadius : {
+        '12px' : '12px',
+        '24px' : '24px'
+      },
     },
   },
   variants: {},
